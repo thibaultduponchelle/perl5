b3793c325b51e9d0806339545456c6abfd92e528
Thibault DUPONCHELLE <thibault.duponchelle@gmail.com>
2020-06-13 14:41:24 +0200

    Haiku compute paths from finddir

--- hints/haiku.sh
+++ hints/haiku.sh
@@ -6,22 +6,9 @@ case "$prefix" in
 *) ;; # pass the user supplied value through
 esac
 
-# libpath
-# $(finddir B_SYSTEM_DEVELOP_DIRECTORY)/lib
-# $(finddir B_USER_DEVELOP_DIRECTORY)/lib
-# /boot/system/develop/lib (new location of /boot/common/lib)
-# /system/lib 
-libpth='/boot/home/config/develop/lib /boot/system/develop/lib /boot/system/non-packaged/lib /system/lib'
-
-# usrinc
-# $(finddir B_SYSTEM_DEVELOP_DIRECTORY)/headers/posix (new location of /boot/develop/headers/posix)
-usrinc='/boot/system/develop/headers/posix'
-
-# locinc
-# $(finddir B_USER_CONFIG_DIRECTORY)/develop/headers
-# /boot/system/non-packaged/headers (new location of /boot/common/include)
-# $(finddir B_SYSTEM_DEVELOP_DIRECTORY)/headers (new location of /boot/develop/headers)
-locinc='/boot/home/config/include /boot/system/non-packaged/include /boot/system/develop/headers'
+libpth="$(finddir B_SYSTEM_DEVELOP_DIRECTORY)/lib $(finddir B_USER_DEVELOP_DIRECTORY)/lib /boot/system/develop/lib /system/lib"
+usrinc="$(finddir B_SYSTEM_DEVELOP_DIRECTORY)/headers/posix"
+locinc="$(finddir B_USER_CONFIG_DIRECTORY)/develop/headers /boot/system/non-packaged/headers $(finddir B_SYSTEM_DEVELOP_DIRECTORY)/headers"
 
 # $(finddir B_SYSTEM_LIB_DIRECTORY)/headers
 libc='/system/lib/libroot.so'
