d854adbd1c98ad2bf8c5187734d087fe6b63274a
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2025-01-20 21:53:31 +0000

    And it all ended in tears

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -833,7 +833,7 @@ our %Modules = (
     },
 
     'Module::CoreList' => {
-        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20241220.tar.gz',
+        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20250120.tar.gz',
         'FILES'        => q[dist/Module-CoreList],
     },
 
